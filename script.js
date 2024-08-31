da60f06b47a73ceabde3ed718a2f9b2613b020f5
Vardaan Bhatia <vardaanbhatia55@gmail.com>
2024-08-31 05:54:24 +0530

    typejs and scroll reveal

--- script.js
+++ script.js
@@ -1,47 +1,48 @@
-// // Mobile menu functionality
-// let menuIcon = document.querySelector("#menu-icon");
-// let navbar = document.querySelector(".navbar");
+//scroll Sections Active Link
 
-// menuIcon.onclick = () => {
-//   menuIcon.classList.toggle("bx-x");
-//   navbar.classList.toggle("active");
-// };
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header nav a");
 
-// // Close menu when a link is clicked
-// let navLinks = document.querySelectorAll(".navbar a");
-// navLinks.forEach((link) => {
-//   link.addEventListener("click", () => {
-//     menuIcon.classList.remove("bx-x");
-//     navbar.classList.remove("active");
-//   });
-// });
+window.onscroll = () => {
+  let top = window.scrollY;
 
-// // Scroll sections
-// let sections = document.querySelectorAll("section");
-// let navLinks = document.querySelectorAll("header nav a");
+  sections.forEach((sec) => {
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
 
-// window.onscroll = () => {
-//   sections.forEach((sec) => {
-//     let top = window.scrollY;
-//     let offset = sec.offsetTop - 100;
-//     let height = sec.offsetHeight;
-//     let id = sec.getAttribute("id");
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach((link) => {
+        link.classList.remove("active");
+      });
+      document
+        .querySelector(`header nav a[href*=${id}]`)
+        .classList.add("active");
+    }
+  });
+};
 
-//     if (top >= offset && top < offset + height) {
-//       navLinks.forEach((links) => {
-//         links.classList.remove("active");
-//         document
-//           .querySelector("header nav a[href*=" + id + "]")
-//           .classList.add("active");
-//       });
-//     }
-//   });
+//scroll Reveal
 
-//   // Sticky header
-//   let header = document.querySelector("header");
-//   header.classList.toggle("sticky", window.scrollY > 100);
+ScrollReveal({
+  distance: "80px",
+  duration: 2000,
+  delay: 200,
+});
+ScrollReveal().reveal(".home-content", { origin: "top" });
+ScrollReveal().reveal(".home-img", { origin: "bottom" });
+ScrollReveal().reveal(".tech-stack", { origin: "left" });
+ScrollReveal().reveal(".my-experience", { origin: "right" });
+ScrollReveal().reveal(".projects .heading", { origin: "top" });
+ScrollReveal().reveal(".image-part", { origin: "left" });
+ScrollReveal().reveal(".activity-box", { origin: "right" });
+ScrollReveal().reveal(".image-container", { origin: "left" });
+ScrollReveal().reveal(".form", { origin: "right" });
 
-//   // Remove toggle icon and navbar when click navbar links (scroll)
-//   menuIcon.classList.remove("bx-x");
-//   navbar.classList.remove("active");
-// };
+//typed animation
+
+var typed = new Typed(".multiple-text", {
+  strings: ["Frontend Developer", "Blooger"],
+  typeSpeed: 100,
+  loop: true,
+});
